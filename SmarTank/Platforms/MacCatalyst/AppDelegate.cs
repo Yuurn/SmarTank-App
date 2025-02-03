0c63f62f951794c72124d226683540ec7184c0aa
Yuurn <90285455+Yuurn@users.noreply.github.com>
2025-02-02 22:27:27 -0500

    Add files via upload

---
+++ SmarTank/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SmarTank
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
